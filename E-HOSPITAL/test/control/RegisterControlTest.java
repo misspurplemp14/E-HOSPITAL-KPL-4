ae93a4672d60dda813795206cf154218919778f5
fathayannisa <fathayannisa@student.telkomuniversity.ac.id>
2021-07-03 14:50:40 +0700

    neww

--- E-HOSPITAL/test/control/RegisterControlTest.java
+++ E-HOSPITAL/test/control/RegisterControlTest.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package control;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 import view.RegisterGUI;
 
-/**
- *
- * @author acer
- */
 public class RegisterControlTest {
     
     @Test
